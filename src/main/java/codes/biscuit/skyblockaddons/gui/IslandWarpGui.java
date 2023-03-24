1924090e59ed5c7d05a856023e3ccb2f38bc8538
half_drop <half_drop@qq.com>
2023-03-24 13:44:05 +0800

    jerry!

--- src/main/java/codes/biscuit/skyblockaddons/gui/IslandWarpGui.java
+++ src/main/java/codes/biscuit/skyblockaddons/gui/IslandWarpGui.java
@@ -306,7 +306,7 @@ public class IslandWarpGui extends GuiScreen {
         PRIVATE_ISLAND("Private Island", 275, 1122),
         GARDEN("Garden", 50, 1000),
         DUNGEON_HUB("Dungeon Hub", 1500, 1050),
-        WINTER_ISLAND("Winter Island", 1470, 850);
+        WINTER_ISLAND("Winter Island", 1600, 850);
 
         private final String label;
         private final int x;
