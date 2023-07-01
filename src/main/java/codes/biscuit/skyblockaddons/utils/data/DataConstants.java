f1ac87e5934e087457eea2e75e356b7fc8882aef
half_drop <1097982745@qq.com>
2023-07-01 11:36:09 +0800

    Update DataConstants.java
    change to www.halfdrop.club

--- src/main/java/codes/biscuit/skyblockaddons/utils/data/DataConstants.java
+++ src/main/java/codes/biscuit/skyblockaddons/utils/data/DataConstants.java
@@ -3,6 +3,6 @@ package codes.biscuit.skyblockaddons.utils.data;
 class DataConstants {
     static final String CDN_BASE_URL = "https://cdn.jsdelivr.net/gh/BiscuitDevelopment/SkyblockAddons-Data@%s/";
     //static final String FALLBACK_CDN_BASE_URL = "https://fastly.jsdelivr.net/gh/BiscuitDevelopment/SkyblockAddons-Data@%s/";
-    static final String FALLBACK_CDN_BASE_URL = "https://gitee.com/halfdrop/SkyblockAddons-Data%s/raw/main/";
+    static final String FALLBACK_CDN_BASE_URL = "https://www.halfdrop.club/SkyblockAddons-Data@";
     static final String GITHUB_BASE_URL = "https://raw.githubusercontent.com/BiscuitDevelopment/SkyblockAddons-Data/main/";
 }
